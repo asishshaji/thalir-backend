e4b5bb854f0fcf92f7996b83ed50ae1c826450dd
Asish <asishshajithomas@gmail.com>
2021-12-12 11:55:23 +0530

    order previwe added, fized bugs

--- services/orderInterface.go
+++ services/orderInterface.go
@@ -4,7 +4,7 @@ import "github.com/asishshaji/thalir-backend/models"
 
 type OServiceInterface interface {
 	CreateOrder(order models.Order) (models.Order, error)
-	GetOrder(oId uint) (models.OrderDetails, error)
+	GetOrder(oId uint) ([]models.OrderItemDisplay, error)
 	GetOrders() ([]models.OrderDetails, error)
 	GetOrdersByDateRange(startDate, endDate string) ([]models.OrderDetails, error)
 }
